bfbb522b920e12aae1bc423747b71d0d816b7261
Tyler House <tyhouse12@gmail.com>
2016-11-20 22:14:02 -0800

    connection manager updated

--- elevated_wh/src/com/cs304/data_managers/ConnectionManager.java
+++ elevated_wh/src/com/cs304/data_managers/ConnectionManager.java
@@ -10,16 +10,63 @@ import java.sql.Statement;
  * Created by tyh0 on 2016-11-20.
  */
 public class ConnectionManager {
-    private Connection con;
 
-    public ConnectionManager() {
+    private String USERNAME = "ora_v1d0b";
+    private String PASSWORD = "a55982145";
+    private Connection connection;
+    private Statement statement;
+
+    public ConnectionManager() { } ;
+
+    public void connectToDb() {
         try {
             DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
-        } catch (SQLException ex) {
-            System.out.println("Message: " + ex.getMessage());
+            connection = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@localhost:1522:ug", USERNAME, PASSWORD
+            );
+            // connection.setAutoCommit(false);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Message: " + e.getMessage());
             System.exit(-1);
         }
+    }
 
+    public void endConnection() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Message: " + e.getMessage());
+            System.exit(-1);
+        }
+    }
+
+    public ResultSet submitQuery(String qstring) {
+        Statement s = null;
+        ResultSet results = null;
+        try {
+            s = connection.createStatement();
+            results = s.executeQuery(qstring);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Message: " + e.getMessage());
+        }
+        return results;
+    }
+
+    public int executeStatement(String cmd){
+        Statement s = null;
+        int rowCount = -1;
+        try {
+            s = connection.createStatement();
+            rowCount = s.executeUpdate(cmd);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Message: " + e.getMessage());
+        }
+        return rowCount;
     }
 
 }
