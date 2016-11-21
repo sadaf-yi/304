4683e1a87191a9963a22cc929db0819380c10cd2
Tyler House <tyhouse12@gmail.com>
2016-11-20 17:12:40 -0800

    tidied directory..... again

---
+++ elevated_wh/src/com/cs304/Main.java
@@ -0,0 +1,8 @@
+package com.cs304;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("This is where the stuff to start the UI will live");
+    }
+}
---
+++ elevated_wh/src/com/cs304/data_managers/ConnectionManager.java
@@ -0,0 +1,25 @@
+package com.cs304.data_managers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Created by tyh0 on 2016-11-20.
+ */
+public class ConnectionManager {
+    private Connection con;
+
+    public ConnectionManager() {
+        try {
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        } catch (SQLException ex) {
+            System.out.println("Message: " + ex.getMessage());
+            System.exit(-1);
+        }
+
+    }
+
+}
