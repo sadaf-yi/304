aaac7fa87c99a9534741f31469eb0725de52eb24
Tyler House <tyhouse12@gmail.com>
2016-11-21 16:21:30 -0800

    DM work on PlacedFor and Product DMs

--- elevated_wh/src/com/cs304/data_managers/PlacedForDM.java
+++ elevated_wh/src/com/cs304/data_managers/PlacedForDM.java
@@ -1,5 +1,7 @@
 package com.cs304.data_managers;
 import com.cs304.data_objects.*;
+
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.sql.ResultSet;
 
@@ -15,4 +17,34 @@ public class PlacedForDM {
         cm = new ConnectionManager();
     }
 
+    public int insertPlacedFor(String orderID, String custID) {
+        String sqlCmd = "INSERT INTO Placed_For(orderID,custID) VALUES(" + orderID + "," + custID +")";
+        cm.connectToDb();
+        int result = cm.executeStatement(sqlCmd);
+        return result;
+    }
+
+    public String[][] getAllPlacedFor() {
+        String sqlQuery = "SELECT * FROM Placed_For";
+        cm.connectToDb();
+        String[][] result = new String[1][1];
+        try {
+            result = cm.submitQuery(sqlQuery);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public String[][] getPlacedForByOrderID(String orderID) {
+        String sqlQuery = "SELECT * FROM Placed_For WHERE orderID="+orderID;
+        cm.connectToDb();;
+        String[][] result = new String[1][1];
+        try {
+            result = cm.submitQuery(sqlQuery);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 }
