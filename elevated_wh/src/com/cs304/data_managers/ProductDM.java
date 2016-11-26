669d5f806bc797e5e6b7ce155623463bc8859b15
sadaf-yi <s.yadegari90@gmail.com>
2016-11-25 18:45:32 -0800

    changes made to productDM to include aggregate functions

--- elevated_wh/src/com/cs304/data_managers/ProductDM.java
+++ elevated_wh/src/com/cs304/data_managers/ProductDM.java
@@ -4,6 +4,10 @@ import com.cs304.data_objects.*;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+/**
+ * Created by tyh0 on 2016-11-20.
+ */
+
 
 public class ProductDM {
     ArrayList<Product> prods;
@@ -31,6 +35,92 @@ public class ProductDM {
         return result;
     }
 
+    /**
+     * deletes products by prodID
+     * @param prodID
+     * @return
+     */
+
+    public int deleteProductbyID(String prodID) {
+        String sqlCmd = "DELETE FROM Product WHERE prodID=" + prodID;
+        cm.connectToDb();
+        int result = cm.executeStatement(sqlCmd);
+        return result;
+    }
+
+    /**
+     * find the cheapest item
+     * @param
+     * @return
+     */
+
+    public String[][] cheapestProd() {
+        String sqlQuery = "SELECT a.prodID, a.prodName, a.prodPrice FROM Product a, Product b GROUP BY a.prodID, a.prodName, a.prodPrice HAVING a.prodPrice = MIN(b.prodPrice)";
+
+        String[][] results = new String[0][0];
+        try {
+            results = cm.submitQuery(sqlQuery);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return results;
+    }
+
+    /**
+     * find the most expensive item
+     * @param
+     * @return
+     */
+
+    public String[][] mostExpensiveProd() {
+        String sqlQuery = "SELECT a.prodID, a.prodName, a.prodPrice FROM Product a, Product b GROUP BY a.prodID, a.prodName, a.prodPrice HAVING a.prodPrice = MAX(b.prodPrice)";
+
+        String[][] results = new String[0][0];
+        try {
+            results = cm.submitQuery(sqlQuery);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return results;
+    }
+
+    /**
+     * find the cheapest product and their recipe ID
+     * @param
+     * @return
+     */
+
+    public String[][] CheapestProdplusRecID() {
+        String sqlQuery = "SELECT a.prodID, a.prodName, a.recID, a.prodPrice FROM Recipe4Product a, Recipe4Product b GROUP BY a.prodID, a.prodPrice, a.prodName, a.recID HAVING a.prodPrice = MIN(b.prodPrice)";
+
+        String[][] results = new String[0][0];
+        try {
+            results = cm.submitQuery(sqlQuery);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return results;
+    }
+
+    /**
+     * find the most expensive product and their recipe ID
+     * @param
+     * @return
+     */
+
+    public String[][] mostExpensiveProdplusRecID() {
+        String sqlQuery = "SELECT a.prodID, a.prodName, a.recID, a.prodPrice FROM Recipe4Product a, Recipe4Product b GROUP BY a.prodID, a.prodPrice, a.prodName, a.recID HAVING a.prodPrice = MAX(b.prodPrice)";
+
+        String[][] results = new String[0][0];
+        try {
+            results = cm.submitQuery(sqlQuery);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return results;
+    }
+
+
 
     /**
      * give the recipe information for a particular prodID
@@ -41,7 +131,7 @@ public class ProductDM {
 
         String[][] recipeResult = new String[0][0];
 
-        String sqlQuery = "SELECT recID, recName, prodecure FROM Recipe4Product";
+        String sqlQuery = "SELECT recID, recName, prodecure FROM Recipe4Product WHERE prodID =" + prodID;
 
         String[][] results = new String[0][0];
         try {
@@ -78,12 +168,20 @@ public class ProductDM {
         int result = cm.executeStatement(sqlCmd);
     }
 
+    /**
+     * Reserve product
+     */
+    /**
+     * gives recipe information and product information for a particular ProdID
+     * @param prodID
+     * @return
+     */
 
-    public String[][] reserveProducts(String prodID,String orderID,  String quantity) {
+    public String[][] getRecInfor4Prod(String prodID,String orderID,  String quantity) {
         decreaseProductStock( prodID,  quantity);
         increaseProdsOfFilled_For( prodID, quantity);
         filledForDM.insertNewFilledFor(prodID, orderID, quantity, "0");
-        String sqlQuery = "SELECT * FROM Filled_For ";
+        String sqlQuery = "SELECT * FROM Filled_For WHERE prodID =" + prodID;
 
         String[][] results = new String[0][0];
         try {
@@ -91,23 +189,6 @@ public class ProductDM {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return results;
-    }
-
-
-    public String[][] getStockProduct(String prodID) {
-        String[][] recipeResult = new String[0][0];
-
-        String sqlQuery = "SELECT stockProduct FROM Product WHERE prodID = " + prodID;
-
-        String[][] results = new String[0][1];
-        try {
-            results = cm.submitQuery(sqlQuery);
-        } catch (SQLException e) {
-            e.printStackTrace();
-
-        }
-        System.out.println(results);
         return results;
     }
 
